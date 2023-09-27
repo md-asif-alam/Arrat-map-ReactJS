9353812055121262be7860b88f9898806be0f03a
Md Asif Alam <mdasifalam910@gmail.com>
2023-09-27 17:15:27 +0530

    map function added

--- arraymap.js
+++ arraymap.js
@@ -3,3 +3,8 @@ const oldarr = ["Md", "Asif", "Alam"];
 console.log(oldarr[0]);
 console.log(oldarr[1]);
 console.log(oldarr[2]);
+
+const newarr = oldarr.map(function (cvalue, i) {
+  return i + ":" + cvalue + "Asif";
+});
+console.log(newarr);
