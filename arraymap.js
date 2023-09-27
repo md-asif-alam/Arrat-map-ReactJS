6cb3d4e0e20f0af2f505f6084513eb8740d6c63a
Md Asif Alam <mdasifalam910@gmail.com>
2023-09-27 17:27:34 +0530

    map function Student Data added

--- arraymap.js
+++ arraymap.js
@@ -1,10 +1,22 @@
-const oldarr = ["Md", "Asif", "Alam"];
-//Before map()
-console.log(oldarr[0]);
-console.log(oldarr[1]);
-console.log(oldarr[2]);
+// const oldarr = ["Md", "Asif", "Alam"];
+// //Before map()
+// console.log(oldarr[0]);
+// console.log(oldarr[1]);
+// console.log(oldarr[2]);
 
-const newarr = oldarr.map(function (cvalue, i) {
-  return i + ":" + cvalue + "Asif";
+// const newarr = oldarr.map(function (cvalue, i) {
+//   return i + ":" + cvalue + "Asif";
+// });
+// console.log(newarr);
+
+const sdata = [
+  { id: 1, name: "Asif", degree: "B.tech" },
+  { id: 2, name: "Alam", degree: "M.tech" },
+  { id: 3, name: "Albert", degree: "PhD" },
+];
+
+const sarr = sdata.map((cvalue) => {
+  return `My name is ${cvalue.name}. and my highest degree is ${cvalue.degree}`;
 });
-console.log(newarr);
+
+console.log(sarr);
